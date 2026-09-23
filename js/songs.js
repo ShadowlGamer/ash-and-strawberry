a294762e07a468d4fcc308cbb8d95450c72c4d3b
ShadowlGamer <69058964+ShadowlGamer@users.noreply.github.com>
2026-09-24 00:40:14 +0300

    upload

---
+++ js/songs.js
@@ -0,0 +1,13 @@
+window.SONGS = {
+  "prologue":   { title: "", url: "" },
+  "chapter1":   { title: "\u044f \u0438 \u0442\u0432\u043e\u0439 \u043a\u043e\u0442 - \u0441\u0432\u0438\u0434\u0430\u043d\u0438\u0435", url: "audio/chapter1.mp3" },
+  "chapter2":   { title: "#3 - aphex twin", url: "audio/chapter2.mp3" },
+  "chapter3":   { title: "forgive - burial", url: "audio/chapter3.mp3" },
+  "chapter3-5": { title: "", url: "" },
+  "chapter4":   { title: "mystery or misery - vegyn", url: "audio/chapter4.mp3" },
+  "chapter5":   { title: "\u0446\u0432\u0435\u0442 \u043d\u0430\u0434\u0435\u0436\u0434\u044b - \u0442\u0430\u043b\u044c\u043d\u0438\u043a", url: "audio/chapter5.mp3" },
+  "chapter6":   { title: "great mother in the sky - lionmilk", url: "audio/chapter6.mp3" },
+  "chapter7":   { title: "forgive - burial", url: "audio/chapter7.mp3" },
+  "chapter8":   { title: "setting sun - you'll never get to heaven", url: "audio/chapter8.mp3" },
+  "chapter9":   { title: "", url: "" }
+};
